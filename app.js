dfc40385c9f06a034b1b4dda924ad8f1dae25557
Manas <manas94gupta@gmail.com>
2017-01-26 13:18:27 +0530

    Add header and footer partials

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ app.get('/', function(req, res) {
 })
 
 app.get('/campsites', function(req, res) {
-    res.render('campsites');
+    var campsites = [
+        {name: 'Barrens', image: 'https://c1.staticflickr.com/6/5204/5315979822_3591b6509f_b.jpg'},
+        {name: 'Desert', image: 'https://c1.staticflickr.com/3/2512/5733464781_8787e851b0_b.jpg'},
+        {name: 'Forest', image: 'https://c1.staticflickr.com/7/6188/6054388099_b8e2f57146_b.jpg'},
+        {name: 'Riverside', image: 'https://c1.staticflickr.com/3/2540/3839041026_01a3941ffa_b.jpg'}
+    ]
+    res.render('campsites', {campsites: campsites});
 })
 
 app.listen('3000', function() {

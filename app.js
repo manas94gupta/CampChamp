e8b12d6c852ab2d1e8f5427c92bfa5ace052a9eb
Manas <manas94gupta@gmail.com>
2017-01-26 12:26:27 +0530

    Setup initial routes and views

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
+app.get('/campsites', function(req, res) {
+    res.render('campsites');
+})
+
 app.listen('3000', function() {
     console.log('Camp Champ serving on port 3000');
 });

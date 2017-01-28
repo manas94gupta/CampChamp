6e4d500b5bb3b8d81f8fae028b84dcbe710142d4
Manas <manas94gupta@gmail.com>
2017-01-28 09:17:21 +0530

    Add a /login route and a basic login page view

--- app.js
+++ app.js
@@ -136,6 +136,11 @@ app.post('/register', function(req, res) {
     });
 });
 
+// show login form
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
 // Serves on port 3000
 app.listen('3000', function() {
     console.log('Camp Champ serving on port 3000');

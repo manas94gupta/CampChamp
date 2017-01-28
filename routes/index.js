1f53faa5ed6443c25bea42f4ca25163042d66d5f
Manas <manas94gupta@gmail.com>
2017-01-28 21:17:20 +0530

    Fix a typo in register user success flash message

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.post('/register', function(req, res) {
             return res.render('register');
         }
         passport.authenticate('local')(req, res, function() {
-            req.flash('success', 'Welcome to CampChamp' + user.username);
+            req.flash('success', 'Welcome to CampChamp ' + user.username);
             res.redirect('/campsites');
         });
     });
